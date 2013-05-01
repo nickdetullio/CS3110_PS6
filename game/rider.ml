5a1d0d62c152c3637a0bc2c1eb94f65e6e4bd116
Nick De Tullio <nrd24@cornell.edu>
2013-04-30 22:59:44 -0400

    deleted  rider.ml

--- game/rider.ml
+++
@@ -1,6 +0,0 @@
-let initialize_rider (id, orientation, modifiers, tile, timer) : rider =
-  rider.id = id;
-  rider.orientation = orientation;
-  rider.modifiers = modifiers;
-  rider.tile = tile;
-  rider.timer = timer;
