120cbf685b1033e9fccf73eb42890574b704c8e9
Chris Cassano <cac366@cornell.edu>
2013-05-02 16:26:51 -0400

    Got BossBot to compile

--- team/bossbot.ml
+++ team/bossbot.ml
@@ -8,7 +8,7 @@ open A_star
 let _ = Random.self_init ()
 
 let item_helper rider c =
-  let res = ref () in
+  let res = ref Success in
   let item_list = ref [] in
   let helper () =
     match c with 
@@ -18,42 +18,40 @@ let item_helper rider c =
   let modifiers = rider.modifiers in
   let helper2 () =
     if (List.mem Invincible modifiers)
-      then if (List.mem Shielded modifiers) then res := ()
-        else if (List.mem Shield !item_list)
+      then if (List.mem Shielded modifiers) then res := Success
+        else if (List.mem_assoc Shield !item_list)
         then res := send_action (UseItem (rider.id, Shield))
-      else res := ()
-    else if (List.mem Invincibility !item_list)
+      else res := Success
+    else if (List.mem_assoc Invincibility !item_list)
       then res := send_action (UseItem (rider.id, Invincibility))
     else if (List.mem Shielded modifiers)
-      then res := ()
-    else if (List.mem Shield !item_list)
-      then res := send_action (UseItem (rider.id, Shielded))
+      then res := Success
+    else if (List.mem_assoc Shield !item_list)
+      then res := send_action (UseItem (rider.id, Shield))
     else () in
   helper2 ();
   let helper3 () = 
-    match res with 
+    match !res with 
     | Success -> print_endline "Item Added Successfully"
-    | Failed -> print_endline "Failed to Add Item"
-    | () -> print_endline "No Item Used" in
+    | Failed -> print_endline "Failed to Add Item" in
   helper3 () 
 
 let item_finder rider =
   let cur_tile = rider.tile in
   let item_locations = snd (List.split !item_locations) in
-  let closest_item = 
-    tile = ref List.hd (item_locations) in
-    for i = 1 to List.length item_locations - 1 do
-      let (c1, r1) = cur_tile in
-      let (c2, r2) = closest_item in
-      let (c3, r3) = List.nth i item_locations in
-      if abs (c1 - c3) < abs (c1 - c2) || abs (r1 - r3) < abs (r1 - r2)
-        then closest_item := List.nth i item_locations
-      else ()
-    done;
-  let path_list = a_star cur_tile closest_item 
+  let tile = ref (List.hd (item_locations)) in
+  for i = 1 to List.length item_locations - 1 do
+    let (c1, r1) = cur_tile in
+    let (c2, r2) = !tile in
+    let (c3, r3) = List.nth item_locations i in
+    if abs (c1 - c3) < abs (c1 - c2) || abs (r1 - r3) < abs (r1 - r2)
+	  then tile := List.nth item_locations i
+    else ()
+  done;
+  let path_list = a_star cur_tile !tile 
     (List.append !red_tail !blue_tail) manhattan in 
   let (c_init, r_init) = cur_tile in
-  let (c_targ, r_targ) = List.head path_list in
+  let (c_targ, r_targ) = List.hd path_list in
   let helper () = 
     if c_init > c_targ 
       then West
@@ -62,7 +60,8 @@ let item_finder rider =
     else if r_init > r_targ
       then North
     else if r_init < r_targ 
-      then South in
+      then South 
+    else rider.orientation in
    helper ()
      
 let new_orientation rider =
@@ -71,38 +70,38 @@ let new_orientation rider =
   let modifiers = rider.modifiers in
   let orient_helper () = 
     if (r > cNUM_ROWS - 2 && dir = South)
-      then if (List.mem (c + 1, r) red_tail || List.mem (c + 1, r) blue_tail 
+      then if (List.mem (c + 1, r) !red_tail || List.mem (c + 1, r) !blue_tail 
         && dir = West) then West else East
     else if (c > cNUM_COLUMNS - 2 && dir = East)
       then if List.mem Invincible modifiers then North 
-      else if (List.mem (c, r - 1) red_tail || List.mem (c, r - 1) blue_tail 
+      else if (List.mem (c, r - 1) !red_tail || List.mem (c, r - 1) !blue_tail 
       && dir = South) then South else North
     else if (r < 2 && dir = West) 
       then if List.mem Invincible modifiers then South
-      else if (List.mem (c, r + 1) red_tail || List.mem (c, r + 1) blue_tail 
+      else if (List.mem (c, r + 1) !red_tail || List.mem (c, r + 1) !blue_tail 
       && dir = South) then North else South
     else if (c < 2 && dir = North)  
       then if List.mem Invincible modifiers then West
-      else if (List.mem (c - 1, r) red_tail || List.mem (c - 1, r) blue_tail 
+      else if (List.mem (c - 1, r) !red_tail || List.mem (c - 1, r) !blue_tail 
       && dir = South) then East else West
-    else if (List.mem (c + 1, r) red_tail || List.mem (c + 1, r) blue_tail 
+    else if (List.mem (c + 1, r) !red_tail || List.mem (c + 1, r) !blue_tail 
       && dir = West && List.mem Invincible modifiers) then West
-    else if (List.mem (c, r + 1) red_tail || List.mem (c, r + 1) blue_tail 
+    else if (List.mem (c, r + 1) !red_tail || List.mem (c, r + 1) !blue_tail 
       && dir = South && List.mem Invincible modifiers) then South
-    else if (List.mem (c - 1, r) red_tail || List.mem (c - 1, r) blue_tail
+    else if (List.mem (c - 1, r) !red_tail || List.mem (c - 1, r) !blue_tail
       && dir = East && List.mem Invincible modifiers) then East
-    else if (List.mem (c, r - 1) red_tail || List.mem (c, r - 1) blue_tail
+    else if (List.mem (c, r - 1) !red_tail || List.mem (c, r - 1) !blue_tail
       && dir = North && List.mem Invincible modifiers) then North
-    else if (List.mem (c + 1, r) red_tail || List.mem (c + 1, r) blue_tail 
+    else if (List.mem (c + 1, r) !red_tail || List.mem (c + 1, r) !blue_tail 
       && dir = West) then South
-    else if (List.mem (c, r + 1) red_tail || List.mem (c, r + 1) blue_tail 
+    else if (List.mem (c, r + 1) !red_tail || List.mem (c, r + 1) !blue_tail 
       && dir = South) then East
-    else if (List.mem (c - 1, r) red_tail || List.mem (c - 1, r) blue_tail
+    else if (List.mem (c - 1, r) !red_tail || List.mem (c - 1, r) !blue_tail
       && dir = East) then North
-    else if (List.mem (c, r - 1) red_tail || List.mem (c, r - 1) blue_tail
+    else if (List.mem (c, r - 1) !red_tail || List.mem (c, r - 1) !blue_tail
       && dir = North) then West 
     else item_finder rider in
-  helper () 
+  orient_helper ()
 
 let bot c =
   while true do
@@ -110,20 +109,20 @@ let bot c =
 			| _ -> failwith "wrong status from game" in	
 		let rider_list,_ = team_data in
 		let res = ref Success in
-    for i = 0 to length rider_list - 1 do
+    for i = 0 to ((List.length rider_list) - 1) do
       let rider = List.nth rider_list i in
       item_helper rider c;
     done;
-    for i = 0 to length rider_list - 1 do
+    for i = 0 to ((List.length rider_list) - 1) do
       let rider = List.nth rider_list i in
 		  res := send_action (ChangeOrientation (rider.id, new_orientation rider));
       let helper () =
-        match res with
-		    | Success -> print_endline "Orient Successful"
+        match !res with
+		  | Success -> print_endline "Orient Successful"
 	      | Failed -> print_endline "Orient Failed" in
       helper ();
     done;
     Thread.delay 0.25
-  done 
+  done
 
 let () = start_bot bot
